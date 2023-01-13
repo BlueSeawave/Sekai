e38c99e0276be642261c2775c9afb2915463b53d
BlueSeawave <blueseawave99@gmail.com>
2023-01-13 20:41:34 +0100

    plz say this works

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
     document.getElementById('test').style.visability='hidden';
     }*/
     function bytbild() {
-        var x = document.getElementById("karta");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
+      var x = document.getElementById("jacuni");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
       }
+    }
     
     /*function bytbild(){
     document.getElementById('test').style.visibility='visable';*/
