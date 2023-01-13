d9fdb09f0e5741d709c9e046b042f5d2d359c2e8
BlueSeawave <blueseawave99@gmail.com>
2023-01-13 19:49:00 +0100

    fixing shit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*function bytbakgrund(){
+    document.getElementById('test').style.visability='hidden';
+    }*/
+    function bytbild() {
+        var x = document.getElementById("karta");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
+    
+    /*function bytbild(){
+    document.getElementById('test').style.visibility='visable';*/
+    
